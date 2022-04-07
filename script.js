fa27063f36f0cf3dc4e0730e0d8d03c2eab65955
Laszlo Kocsis <leslie.wilmore00@gmail.com>
2022-04-07 23:14:57 +0300

    Refine content

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ const content = document.querySelector('.content');
 
 const total = 32;
 
-const titleText = 'Title Text';
-let buttonText = 'Button Text';
+const titleText = 'Product #';
+let buttonText = 'Add to cart';
 
 
 for (let i = 0; i < total; i++) {
@@ -17,7 +17,7 @@ for (let i = 0; i < total; i++) {
 
   const title = document.createElement('p');
   title.classList.add('item-title');
-  title.textContent = titleText;
+  title.textContent = titleText + (i + 1);
   item.appendChild(title);
 
   const btn = document.createElement('button');
