22736106c8d45734adef8266c59abfcd954dfca5
gyrajmi <gyorgy.rajmund@student.ms.sapientia.ro>
2022-05-20 12:45:20 +0300

    Vázlat

---
+++ test.js
@@ -0,0 +1,20 @@
+
+var ind = 0;
+var paragraphs;
+
+function changeP(to) {
+    paragraphs[ind].style.display = 'none';
+    paragraphs[ind = (paragraphs.length + ind + to) % paragraphs.length].style.display = 'block';
+}
+
+window.onload = () => {
+    paragraphs = Array.from($('bal').children).map(p => { p.style.display = 'none'; return p; });
+    changeP(0);
+    $('gombok').previous.addEventListener('click', () => changeP(-1));
+    $('gombok').next.addEventListener('click', () => changeP(+1));
+    console.debug(paragraphs);
+    
+}
+
+
+
